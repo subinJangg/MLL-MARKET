73d714a7184fa536de05bfa3d409af97d93cd541
SUBINJANG <molly1998@naver.com>
2023-06-13 13:46:20 +0900

    230613 git test

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- <PageHeader/> 헤더 컴포넌트 -->
+  <!-- <router-view/>  페이지 이동이 표시될 곳 -->
+  <!-- <PageFooter/> 푸터 컴포넌트 -->
+  <HelloWorld/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Home',
+    props: {
+
+    }
+  }
+  </script>
+  
+  <style scoped>
+ 
+  </style>
+  
